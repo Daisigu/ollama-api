feff90771d641942a8a2da68c8450eff0511ff59
Богдан Гоголь <bogd2288@gmail.com>
2024-11-08 17:47:44 +0300

    deps, tsconfig, server

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,41 @@
-import express, { Request, Response } from 'express';
-import axios from 'axios';
+import express, { Request, Response } from "express";
+import ollama from "ollama";
 
 const app = express();
 const port = 3001;
 
 app.use(express.json());
 
-app.post('/ollama', async (req: Request, res: Response): Promise<void> => {
-  
+app.post("/ollama", async (req: Request, res: Response): Promise<void> => {
   const { prompt } = req.body;
 
   if (!prompt) {
-    res.status(400).json({ error: 'Prompt is required' });
+    res.status(400).json({ error: "Prompt is required" });
     return;
   }
 
   try {
-    const ollamaResponse = await axios.post('http://localhost:11434/infer', {
-      prompt,
+    const response = await ollama.chat({
+      model: "llama3.2-vision",
+      messages: [
+        {
+          role: "user",
+          content: "What is in this image?",
+          images: ["image.jpg"],
+        },
+      ],
     });
 
-    res.json({ response: ollamaResponse.data });
+    res.json({ response: response });
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Something went wrong with Ollama' });
+    console.error("Error:", error);
+    res.status(500).json({ error: "Something went wrong with Ollama" });
   }
 });
-app.get('/status', (req: Request, res: Response): void => {
-  res.json({ status: 'Server is running', message: 'Ollama API is available' });
+app.get("/status", (req: Request, res: Response): void => {
+  res.json({ status: "Server is running", message: "Ollama API is available" });
 });
 
-app.listen(port, '0.0.0.0', async () => {
+app.listen(port, "0.0.0.0", async () => {
   console.log(`Server is running at http://0.0.0.0:${port}`);
 });
